696a3bd4186a9d8fbf0492e1c56888935f1d6128
chenyunguiMilook <linxitj2003@icloud.com>
2025-03-06 09:55:02 +0800

    fixed compile issue

--- Sources/SwiftInit/Protocols.swift
+++ Sources/SwiftInit/Protocols.swift
@@ -29,14 +29,6 @@ public protocol DeclConvertible: CodeConvertible {
     
 }
 
-//public protocol ShapeDeclConvertible {
-//    var bounds: CGRect { get }
-//    func uiBezierPathDecl() -> VarDecl
-//    func swiftUIPathDecl() -> VarDecl
-//    func swiftUIShapeDecl(in bounds: CGRect?) -> VarDecl
-//    func applying(_ transform: Matrix2D) -> Self
-//}
-
 // MARK: - MultiCodeConvertible
 public protocol SourceKind: CustomStringConvertible, CaseIterable, Sendable {
 }
@@ -46,23 +38,4 @@ public protocol MultiCodeConvertible {
     var snippetGroupName: String { get }
     func code(for kind: Kind) -> CodeConvertible?
 }
-
-extension MultiCodeConvertible {
-    
-    public var snippetGroup: SnippetGroup {
-        SnippetGroup(name: snippetGroupName, snippets: snippets())
-    }
-    
-    public func snippets() -> [CodeSnippet] {
-        snippets(for: Array(Kind.allCases))
-    }
-
-    public func snippets(for kinds: [Kind]) -> [CodeSnippet] {
-        kinds.compactMap { kind in
-            guard let source = self.code(for: kind) else { return nil }
-            return CodeSnippet(kind.description, source)
-        }
-    }
-}
-
 
